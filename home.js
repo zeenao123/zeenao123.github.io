f0843cd3aebc15ee6b9ee381bec1f627ecb37de3
Zeena Omer <niyaomer@gmail.com>
2019-07-10 21:36:00 -0400

    this is my first commit!

---
+++ home.js
@@ -0,0 +1,13 @@
+
+console.log("Test123");
+
+function change_image() {
+
+  var image = document.getElementById('rory_lor');
+  image.src="img/gilmore.jpg";
+}
+
+function increase_size() {
+  var image = document.getElementById('rory_lor');
+  image.style.width = "30%"
+}
